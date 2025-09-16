84f667fd2887a448b781a836e25da6c765eb152a
Tsoorh <tsoor.hartov@gmail.com>
2025-09-16 08:43:54 +0300

    fix query book service

--- services/book.service.js
+++ services/book.service.js
@@ -44,7 +44,7 @@ function query(filterBy = {}) {
                     break;
                 }
             }
-            if((filterBy.category).toLowerCase()!=="all"){ 
+            if((filterBy.category)!=="All"){ 
                 const regExp = new RegExp(filterBy.category, 'i')
                 books = books.filter(book => regExp.test(book.categories))
             }
